1dacd77c301c7f3a2f392750f296df60c90465c3
gujitao001 <gujitao001@ke.com>
2019-03-21 15:53:51 +0800

    引入claw_plugin

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -1,12 +1,16 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+#include <claw_plugin/ClawPlugin.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+    
+    [ClawPlugin registerWithRegistrar:[self registrarForPlugin:@"ClawnPlugin"]];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
